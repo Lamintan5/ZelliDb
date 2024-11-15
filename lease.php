a8bf6b060b436fd9fa9f0b9e7486fdfd97e84ecd
Lamintan5 <tech@studio5ive.org>
2024-11-15 21:35:48 +0300

    get previous tenant

--- lease.php
+++ lease.php
@@ -14,6 +14,33 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if('GET_PREVIOUS_TENANTS' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $uid = $_POST['uid'];
+        $query = "SELECT * FROM $table WHERE uid = '".$uid."' AND end != ''";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
+
+    if('GET_CURRENT_BY_ENTITY' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $eid = $_POST['eid'];
+        $query = "SELECT * FROM $table WHERE eid = '".$eid."' AND end = ''";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
 
     if('GET_CURRENT' == $action){
         if ($db->connect_errno) {
