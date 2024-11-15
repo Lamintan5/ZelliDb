ae4d6458b326dd2cd3bb9d285a7a3d25db63a88e
Lamintan5 <tech@studio5ive.org>
2024-11-15 21:34:37 +0300

    update details

--- lease.php
+++ lease.php
@@ -14,6 +14,21 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if('UPDATE_DETAILS' == $action){
+        $lid = $_POST['lid'];
+        $rent = $_POST['rent'];
+        $deposit = $_POST['deposit'];
+        $start = $_POST['start'];
+        $end = $_POST['end'];
+        $sql = "UPDATE $table SET rent = '$rent', deposit = '$deposit', start = '$start',  end = '$end' WHERE lid = '$lid'";
+        if ($conn->query($sql) === TRUE) { 
+            echo "success";
+        } else {
+            echo "error";
+        }
+        $conn->close();
+        return;
+    }
 
     if('UPDATE_ALL_TENANTS' == $action){
         $eid = $_POST['eid'];
