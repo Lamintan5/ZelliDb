dc5ac302cbcb6c32b23eddd2271b7d0ce979db35
Lamintan5 <tech@studio5ive.org>
2024-11-15 21:35:00 +0300

    get one

--- lease.php
+++ lease.php
@@ -14,6 +14,21 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if('GET_ONE' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $tid = $_POST['tid'];
+        $query = "SELECT * FROM $table WHERE tid = '".$tid."'";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
+
+   
 
     if('UPDATE' == $action){
         $lid = $_POST['lid'];
