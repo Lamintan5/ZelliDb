b72ad4bf22c10da6e832b86ed6f20b1ca6f88cac
Lamintan5 <tech@studio5ive.org>
2024-11-18 21:44:57 +0300

    update

--- unit.php
+++ unit.php
@@ -12,6 +12,32 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if('UPDATE' == $action){
+        $id = $_POST['id'];
+        $title = $_POST['title'];
+        $room = $_POST['room'];
+        $price = $_POST['price'];
+        $deposit = $_POST['deposit'];
+
+        $checkSql = "SELECT COUNT(*) as count FROM $table WHERE id = '$id'";
+        $result = $conn->query($checkSql);
+        $row = $result->fetch_assoc();
+
+        if ($row['count'] == 0) {
+            echo "Does not exist";
+        } else {
+            $sql = "UPDATE $table SET title = '$title', room = '$room', price = '$price', deposit = '$deposit' WHERE id = '$id'";
+            if ($conn->query($sql) === TRUE) { 
+                echo "success";
+            } else {
+                echo "error";
+            }
+        }
+
+
+        $conn->close();
+        return;
+    }
 
     if('UPDATE_ALL_UNIT' == $action){
         $eid = $_POST['eid'];
