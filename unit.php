d0f70d306dd4f30001700b0360e9e6673538cb21
Lamintan5 <tech@studio5ive.org>
2024-11-18 21:45:13 +0300

    UPDATE_TID

--- unit.php
+++ unit.php
@@ -12,6 +12,59 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+
+
+    if (isset($action) && $action === 'UPDATE_TID') {
+        $id = $_POST['id'];
+        $tid = $_POST['tid'];
+        $lid = $_POST['lid'];
+    
+        $id = $conn->real_escape_string($id);
+        $tid = $conn->real_escape_string($tid);
+        $lid = $conn->real_escape_string($lid);
+    
+        if ($tid === "" && $lid === "") {
+            $updateSql = "UPDATE $table SET `tid` = '', `lid` = '' WHERE id = '$id'";
+            if ($conn->query($updateSql) === TRUE) {
+                echo "success";
+            } else {
+                echo "error";
+            }
+        } else {
+            $sql = "SELECT `tid` FROM $table WHERE id = '$id'";
+            $result = $conn->query($sql);
+    
+            if ($result && $result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $tidField = $row['tid'];
+    
+                if (empty($tidField)) {
+                    $newTidField = $tid;
+                } else {
+                    $tidsArray = explode(',', $tidField);
+                    if (!in_array($tid, $tidsArray)) {
+                        $newTidField = $tidField . ',' . $tid;
+                    } else {
+                        $newTidField = $tidField;
+                    }
+                }
+    
+                $updateSql = "UPDATE $table SET `tid` = '$newTidField', `lid` = '$lid' WHERE id = '$id'";
+                if ($conn->query($updateSql) === TRUE) {
+                    echo "success";
+                } else {
+                    echo "error";
+                }
+            } else {
+                echo "Does not exist";
+            }
+        }
+    
+        // Close the connection
+        $conn->close();
+    }
+    
+
     if('UPDATE' == $action){
         $id = $_POST['id'];
         $title = $_POST['title'];
