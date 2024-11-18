bce694dc362814bc266472e7e8b7a1b779333aa3
Lamintan5 <tech@studio5ive.org>
2024-11-18 21:44:48 +0300

    update all

--- unit.php
+++ unit.php
@@ -13,7 +13,40 @@
     }
 
 
+    if('UPDATE_ALL_UNIT' == $action){
+        $eid = $_POST['eid'];
+        $tid = $_POST['tid'];
+        $sql = "UPDATE $table SET tid = '$tid' WHERE eid = '$eid'";
+        if ($conn->query($sql) === TRUE) { 
+            echo "success";
+        } else {
+            echo "error";
+        }
+        $conn->close();
+        return;
+    }
 
+    if('DELETE' == $action){
+        $id = $_POST['id'];
+
+        $checkSql = "SELECT COUNT(*) as count FROM $table WHERE id = '$id'";
+        $result = $conn->query($checkSql);
+        $row = $result->fetch_assoc();
+
+        if ($row['count'] == 0) {
+            echo "Does not exist";
+        } else {
+            $sql = "DELETE FROM $table WHERE id = '$id'";
+            if ($conn->query($sql) === TRUE) {
+                echo "success";
+            } else {
+                echo "error";
+            }
+        }
+
+        $conn->close();
+        return;
+    }
 
     if('DELETE_ALL' == $action){
         $eid = $_POST['eid'];
