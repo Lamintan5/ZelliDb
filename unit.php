bacc24a12bdab52343c5b7ac7d6d1805b79f55e4
Lamintan5 <tech@studio5ive.org>
2024-11-18 21:47:30 +0300

    Get All

--- unit.php
+++ unit.php
@@ -13,6 +13,18 @@
     }
 
 
+    if('GET_ALL' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $query = "SELECT * FROM $table";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
 
     if('GET_CURRENT' == $action){
         if ($db->connect_errno) {
