77e96074b2f47a3f6cc4928e94b097aced5138de
Lamintan5 <tech@studio5ive.org>
2024-11-20 22:49:25 +0300

    storage

--- routes.js
+++ routes.js
@@ -1,4 +1,14 @@
 
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + ".jpeg");
+    },
+});
+
 const upload = multer({
     storage: storage,
 });
