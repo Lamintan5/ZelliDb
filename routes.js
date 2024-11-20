5896b711baa6f86082ef1023b89aca46539e4bb3
Lamintan5 <tech@studio5ive.org>
2024-11-20 22:49:04 +0300

    add image

--- routes.js
+++ routes.js
@@ -1,1 +1,15 @@
 
+
+router.route("/addimage").post(upload.single("img"), (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: "No file uploaded." });
+        }
+        res.json({ path: req.file.filename });
+    } catch (e) {
+        console.error("Error uploading image:", e);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+module.exports = router;
