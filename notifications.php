1c376b3d79274f4a5ec36410eccb1f603fba5974
Lamintan5 <tech@studio5ive.org>
2024-11-23 10:17:45 +0300

    update seen

--- notifications.php
+++ notifications.php
@@ -13,6 +13,42 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if ('UPDATE_SEEN' == $action) {
+        $nid = $_POST['nid'];
+        $uid = $_POST['uid'];
+    
+        $sql = "SELECT `seen` FROM $table WHERE nid = '$nid'";
+        $result = $conn->query($sql);
+        
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $seenField = $row['seen'];
+    
+            if (empty($seenField)) {
+                $newSeenField = $uid;
+            } else {
+                $uidsArray = explode(',', $seenField);
+                if (!in_array($uid, $uidsArray)) {
+                    $newSeenField = $seenField . ',' . $uid;
+                } else {
+                    $newSeenField = $seenField;
+                }
+            }
+
+            $updateSql = "UPDATE $table SET `seen` = '$newSeenField' WHERE nid = '$nid'";
+            if ($conn->query($updateSql) === TRUE) {
+                echo "success";
+            } else {
+                echo "failed";
+            }
+        } else {
+            echo "Does not exist";
+        }
+    
+        $conn->close();
+        return;
+    }
+
     if('DELETE' == $action){
         $nid = $_POST['nid'];
         $sql = "DELETE FROM $table WHERE nid = '$nid'";
