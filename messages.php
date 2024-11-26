e2e8c3b0b8dbcccbb6777d3d6d47f9735e0fdea9
Lamintan5 <tech@studio5ive.org>
2024-11-26 18:50:15 +0300

    delete

--- messages.php
+++ messages.php
@@ -13,6 +13,19 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+
+    if('DELETE' == $action){
+        $did = $_POST['did'];
+        $sql = "DELETE FROM $table WHERE did = '$did'";
+        if ($conn->query($sql) === TRUE) {
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
+
     if('DELETE_ALL' == $action){
         $eid = $_POST['eid'];
         $sql = "DELETE FROM $table WHERE eid = '$eid'";
