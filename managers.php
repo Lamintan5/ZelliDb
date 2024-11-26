dd596706a2597ce133e356a0d07a509da4052ae7
Lamintan5 <tech@studio5ive.org>
2024-11-26 18:53:27 +0300

    get all

--- managers.php
+++ managers.php
@@ -14,6 +14,18 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if('GET_ALL' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $query = "SELECT * FROM $table";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
 
     if('GET_CURRENT' == $action){
         if ($db->connect_errno) {
