5b8dd66e6fd5006d1ec358702e6c3a705c25c437
Lamintan5 <tech@studio5ive.org>
2024-11-27 12:53:37 +0300

    remove cotid

--- lease.php
+++ lease.php
@@ -15,6 +15,50 @@
     }
 
 
+
+    if (isset($action) && $action === 'REMOVE_COTID') {
+        $lid = $_POST['lid'];
+        $ctid = $_POST['tid'];
+    
+        $lid = $conn->real_escape_string($lid);
+        $ctid = $conn->real_escape_string($ctid);
+    
+        $sql = "SELECT `ctid` FROM $table WHERE lid = '$lid'";
+        $result = $conn->query($sql);
+    
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $ctidField = $row['ctid'];
+    
+            if (!empty($ctidField)) {
+                $ctidsArray = explode(',', $ctidField);
+                $ctidsArray = array_filter($ctidsArray, function ($item) use ($ctid) {
+                    return $item != $ctid;
+                });
+                $newCtidField = implode(',', $ctidsArray);
+            } else {
+                $newCtidField = ''; 
+            }
+    
+            $updateSql = "UPDATE $table SET `ctid` = '$newCtidField' WHERE lid = '$lid'";
+            if ($conn->query($updateSql) === TRUE) {
+                $updSql = "UPDATE payments SET `tid` = '$newCtidField' WHERE lid = '$lid'";
+                if ($conn->query($updSql) === TRUE) {
+                    echo "success";
+                } else {
+                    echo "error" . $conn->error;
+                }
+            } else {
+                echo "error" . $conn->error;
+            }
+        } else {
+            echo "Does not exist";
+        }
+        $conn->close();
+        return;
+    }
+    
+
     if('DELETE' == $action){
         $id = $_POST['tid'];
         $sql = "DELETE FROM $table WHERE tid = '$tid'";
