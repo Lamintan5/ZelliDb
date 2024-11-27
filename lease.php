d8c1c25436ee660a5777fa00ddb930fb369d420e
Lamintan5 <tech@studio5ive.org>
2024-11-27 12:55:46 +0300

    get all

--- lease.php
+++ lease.php
@@ -15,6 +15,20 @@
     }
 
 
+
+    if('GET_ALL' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $query = "SELECT * FROM $table";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
+
     if('GET_PREVIOUS_TENANTS' == $action){
         if ($db->connect_errno) {
             die("Failed to connect to MySQL: " . $db->connect_error);
