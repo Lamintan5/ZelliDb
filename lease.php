3e7beeb218249d6c74b98366311cf68c11e1692a
Lamintan5 <tech@studio5ive.org>
2024-11-27 12:55:00 +0300

    get current

--- lease.php
+++ lease.php
@@ -16,6 +16,20 @@
 
 
 
+    if('GET_CURRENT' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $pid = $_POST['pid'];
+        $query = "SELECT * FROM $table WHERE FIND_IN_SET('" . $pid . "', pid)";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);    
+    }
+
     if('GET_PREVIOUS_BY_ENTITY' == $action){
         if ($db->connect_errno) {
             die("Failed to connect to MySQL: " . $db->connect_error);
