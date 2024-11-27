afee9da27d7a136f8ffab220a10358bac5498c7a
Lamintan5 <tech@studio5ive.org>
2024-11-27 12:53:25 +0300

    delete

--- lease.php
+++ lease.php
@@ -14,7 +14,18 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-  
+
+    if('DELETE' == $action){
+        $id = $_POST['tid'];
+        $sql = "DELETE FROM $table WHERE tid = '$tid'";
+        if ($conn->query($sql) === TRUE) {
+            echo "success";
+        } else {
+            echo "error";
+        }
+        $conn->close();
+        return;
+    }    
 
     if('DELETE_ALL' == $action){
         $eid = $_POST['eid'];
