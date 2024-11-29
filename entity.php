30d63c3abf9bee1915935bcc3ac6d4e3430d7b42
Lamintan5 <tech@studio5ive.org>
2024-11-30 01:10:09 +0300

    get all

--- entity.php
+++ entity.php
@@ -13,6 +13,22 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if('GET_ALL' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $query = "SELECT * FROM $table";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
+
+  
+
+
     if ('UPDATE_PID' == $action) {
         $eid = $_POST['eid'];
         $uid = $_POST['uid'];
