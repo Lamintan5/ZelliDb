c6b21b2dfde852ba08e27f5fe2c39b03ec211d0c
Lamintan5 <tech@studio5ive.org>
2024-11-30 01:07:25 +0300

    delete

--- entity.php
+++ entity.php
@@ -13,5 +13,16 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if('DELETE' == $action){
+        $eid = $_POST['eid'];
+        $sql = "DELETE FROM $table WHERE eid = '$eid'";
+        if ($conn->query($sql) === TRUE) {
+            echo "success";
+        } else {
+            echo "error";
+        }
+        $conn->close();
+        return;
+    }
     
 ?>
