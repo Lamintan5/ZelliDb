854b380f101b14539d95ddb0c65eb88c94383dd9
Lamintan5 <tech@studio5ive.org>
2024-11-30 01:07:43 +0300

    update

--- entity.php
+++ entity.php
@@ -13,6 +13,39 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+
+    if('UPDATE' == $action){
+        $image = $_FILES['image']['name'];
+        $eid = $_POST['eid'];
+        $title = $_POST['title'];
+        $location = $_POST['location'];
+        $category = $_POST['category'];
+        $due = $_POST['due'];
+        $late = $_POST['late'];        
+    
+        if (isset($_FILES['image']) && !empty($_FILES['image']['name'])) {
+            $image = $_FILES['image']['name'];
+            $tmp_name = $_FILES['image']['tmp_name'];
+            $imagePath = 'logos/' . $image;
+            move_uploaded_file($tmp_name, $imagePath);
+        } else {
+            $image = $_POST['image']; 
+        }
+        $sql = "UPDATE $table SET title = '$title', location = '$location', category = '$category', due = '$due', late = '$late', image = '$image'";
+       
+
+        $sql .= " WHERE eid = '$eid'";
+        if ($conn->query($sql) === TRUE) { 
+            echo "success";
+        } else {
+            echo "error";
+        }
+
+        $conn->close();
+        return;
+    }
+
+
     if('UPDATE_UTIL' == $action){
         $eid = $_POST['eid'];
         $utilities = $_POST['utilities'];
