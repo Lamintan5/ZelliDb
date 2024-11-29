b1f6994de4f54e6caec08c578be282a3223b3eb7
Lamintan5 <tech@studio5ive.org>
2024-11-30 01:07:35 +0300

    update util

--- entity.php
+++ entity.php
@@ -13,6 +13,32 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    if('UPDATE_UTIL' == $action){
+        $eid = $_POST['eid'];
+        $utilities = $_POST['utilities'];
+
+        $checkSql = "SELECT COUNT(*) as count FROM $table WHERE eid = '$eid'";
+        $result = $conn->query($checkSql);
+        $row = $result->fetch_assoc();
+
+        if ($row['count'] == 0) {
+            echo "Does not exist";
+        } else {
+            $sql = "UPDATE $table SET utilities = '$utilities' WHERE eid = '$eid'";
+            if ($conn->query($sql) === TRUE) { 
+                echo "success";
+            } else {
+                echo "error";
+            }
+        }
+
+
+        $conn->close();
+        return;
+    }
+
+
+
     if('DELETE' == $action){
         $eid = $_POST['eid'];
         $sql = "DELETE FROM $table WHERE eid = '$eid'";
