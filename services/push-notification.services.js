0f494e2042a97b283e6578f6f5f203d871a92cd1
Lamintan5 <tech@studio5ive.org>
2024-12-01 17:57:07 +0300

    http var

--- services/push-notification.services.js
+++ services/push-notification.services.js
@@ -2,6 +2,14 @@ const {ONE_SIGNAL_CONFIG} = require("../config/app.config");
 
 async function SendNotification(data, callback){
     
+
+    var https = require("https");
+    var req = https.request(options, function(res){
+        res.on("data", function(data){
+            console.log(JSON.parse(data));
+            return callback(null, JSON.parse(data));
+        });
+    });
     req.on("error", function(e){
         return callback({
             message: e
