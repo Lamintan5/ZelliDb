bdbb737ab808ac79342fa9cac3bd0534cfe947b5
Lamintan5 <tech@studio5ive.org>
2024-12-01 17:57:17 +0300

    options

--- services/push-notification.services.js
+++ services/push-notification.services.js
@@ -3,6 +3,14 @@ const {ONE_SIGNAL_CONFIG} = require("../config/app.config");
 async function SendNotification(data, callback){
     
 
+    var options = {
+        host: "onesignal.com",
+        port: 443,
+        path: "/api/v1/notifications",
+        method: "POST",
+        headers: headers
+    };
+
     var https = require("https");
     var req = https.request(options, function(res){
         res.on("data", function(data){
