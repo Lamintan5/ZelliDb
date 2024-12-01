25b3b7e3991603ff3d9eb55b893549231f2d2670
Lamintan5 <tech@studio5ive.org>
2024-12-01 17:55:46 +0300

    get my

--- duties.php
+++ duties.php
@@ -14,6 +14,20 @@
     }
 
 
+    if('GET_MY' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $pid = $_POST['pid'];
+        $query = "SELECT * FROM $table WHERE pid = '".$pid."'";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
+
     if('GET_CURRENT' == $action){
         if ($db->connect_errno) {
             die("Failed to connect to MySQL: " . $db->connect_error);
