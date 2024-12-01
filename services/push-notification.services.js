6aaed2c09fdd3a65100bce1e591dab682079e139
Lamintan5 <tech@studio5ive.org>
2024-12-01 17:56:58 +0300

    on error

--- services/push-notification.services.js
+++ services/push-notification.services.js
@@ -2,6 +2,11 @@ const {ONE_SIGNAL_CONFIG} = require("../config/app.config");
 
 async function SendNotification(data, callback){
     
+    req.on("error", function(e){
+        return callback({
+            message: e
+        });
+    });
 
     req.write(JSON.stringify(data));
     req.end();
