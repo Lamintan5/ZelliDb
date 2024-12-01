45f937736613f5dfe7df7e2cc1cb6525677ccae4
Lamintan5 <tech@studio5ive.org>
2024-12-01 17:55:31 +0300

    update

--- duties.php
+++ duties.php
@@ -13,6 +13,20 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+
+    if('UPDATE' == $action){
+        $did = $_POST['did'];
+        $duties = $_POST['duties'];  
+        $sql = "UPDATE $table SET  duties = '$duties' WHERE did = '$did'";
+        if ($conn->query($sql) === TRUE) { 
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
+
     if('DELETE' == $action){
         $did = $_POST['did'];
         $sql = "DELETE FROM $table WHERE did = '$did'";
