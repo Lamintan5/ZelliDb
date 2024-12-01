47e73f5e8cd95f1a0e1a8510b512357c724d58c4
Lamintan5 <tech@studio5ive.org>
2024-12-01 17:56:50 +0300

    req

--- services/push-notification.services.js
+++ services/push-notification.services.js
@@ -2,6 +2,9 @@ const {ONE_SIGNAL_CONFIG} = require("../config/app.config");
 
 async function SendNotification(data, callback){
     
+
+    req.write(JSON.stringify(data));
+    req.end();
 }
 
 module.exports = {
