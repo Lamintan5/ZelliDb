1d833c645856bedcd0c807fe8d12bddbfc878b01
Lamintan5 <tech@studio5ive.org>
2024-12-03 19:15:31 +0300

    verify otp

--- services/otp.service.js
+++ services/otp.service.js
@@ -4,6 +4,21 @@ const key = "Lamintan";
 const emailServices = require("../services/emailer.service");
 
 
+async function verifyOTP(params, callback){
+    let [hashValue, expires] = params.hash.split('.');
+
+    let now = Date.now();
+
+    if(now > parseInt(expires)) return callback("OTP Expired");
+
+    let data = `${params.email}.${params.otp}.${expires}`;
+    let newCalculateHash = crypto.createHmac("sha256", key).update(data).digest("hex");
+    if(newCalculateHash === hashValue) {
+        return callback(null, "Success");
+    }
+
+    return callback("Invalid OTP");
+}
 
 module.exports = {
     sendOTP,
