9d260032e2e2622625b2021ece1689226e8a2a0b
Lamintan5 <tech@studio5ive.org>
2024-12-04 16:34:57 +0300

    sendMail

--- services/emailer.service.js
+++ services/emailer.service.js
@@ -2,6 +2,13 @@ var nodemailer = require('nodemailer');
 
 async function sendEmail(params, callback){
    
+    transport.sendMail(mailOptions, function (error, info){
+        if(error){
+            return callback(error);
+        } else {
+            return callback(null, info.response);
+        }
+    });
 }
 
 module.exports = {
