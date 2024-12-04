fc62ff69177cbe068f4d6a4c84967f2609f1d4ca
Lamintan5 <tech@studio5ive.org>
2024-12-04 16:36:32 +0300

    SendNotificationToDevice

--- contollers/push-notification.controller.js
+++ contollers/push-notification.controller.js
@@ -1,3 +1,7 @@
 const { ONE_SIGNAL_CONFIG } = require("../config/app.config")
 const pushNotificationService = require("../services/push-notification.services.js");
 
+
+exports.SendNotificationToDevice = (req, res, next) => {
+    
+};
