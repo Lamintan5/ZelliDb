cd5a6fdd07e5ae73b89d1208c2f0eff47350d8ff
Lamintan5 <tech@studio5ive.org>
2024-12-04 16:35:05 +0300

    mailOptions

--- services/emailer.service.js
+++ services/emailer.service.js
@@ -2,6 +2,15 @@ var nodemailer = require('nodemailer');
 
 async function sendEmail(params, callback){
    
+
+    var mailOptions = {
+        from: 'billclinton1758@gmail.com',
+        to: params.email,
+        subject: params.subject,
+        text: params.body,
+        html: params.html,
+    };
+
     transport.sendMail(mailOptions, function (error, info){
         if(error){
             return callback(error);
