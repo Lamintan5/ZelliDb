ae6f7ddba7e1f1fc381efc0bb388062103707339
Lamintan5 <tech@studio5ive.org>
2024-12-04 16:37:06 +0300

    SendNotification

--- contollers/push-notification.controller.js
+++ contollers/push-notification.controller.js
@@ -3,6 +3,16 @@ const pushNotificationService = require("../services/push-notification.services.
 
 exports.SendNotification = (req, res, next) => {
     
+
+    pushNotificationService.SendNotification(message, (error, results) => {
+        if(error){
+            return next(error);
+        }
+        return res.status(200).send({
+            message: "Success",
+            data: results
+        });
+    });
 };
 
 
