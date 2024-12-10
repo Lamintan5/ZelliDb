8749f9a6309bb113d84b39201fbe185e02231279
Lamintan5 <tech@studio5ive.org>
2024-12-10 13:27:56 +0300

    update

--- billing.php
+++ billing.php
@@ -1,6 +1,21 @@
 <?php
     
 
+    if('UPDATE' == $action){
+        $bid = $_POST['bid'];
+        $businessno = $_POST['businessno'];  
+        $account = $_POST['account'];
+        $type = $_POST['type'];    
+        $sql = "UPDATE $table SET  businessno = '$businessno' WHERE bid = '$bid'";
+        if ($conn->query($sql) === TRUE) { 
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
+
     if('DELETE' == $action){
         $bid = $_POST['bid'];
         $sql = "DELETE FROM $table WHERE bid = '$bid'";
