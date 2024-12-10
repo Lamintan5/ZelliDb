57597973d5a0d34340d20d31e1bcdbaf869e8a28
Lamintan5 <tech@studio5ive.org>
2024-12-10 13:27:48 +0300

    delete

--- billing.php
+++ billing.php
@@ -1,4 +1,16 @@
 <?php
     
 
+    if('DELETE' == $action){
+        $bid = $_POST['bid'];
+        $sql = "DELETE FROM $table WHERE bid = '$bid'";
+        if ($conn->query($sql) === TRUE) {
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
+
 ?>
