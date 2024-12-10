dcf0ba116ad09fd7d644880600290b03e4a2698b
Lamintan5 <tech@studio5ive.org>
2024-12-10 13:28:07 +0300

    add

--- billing.php
+++ billing.php
@@ -1,6 +1,33 @@
 <?php
     
 
+    if('ADD' == $action){
+        $bid = $_POST['bid'];
+        $eid = $_POST['eid'];
+        $pid = $_POST['pid'];
+        $bill = $_POST['bill'];
+        $businessno = $_POST['businessno'];
+        $account = $_POST['account'];
+        $type = $_POST['type'];
+
+        $sql = "SELECT bid FROM $table WHERE bid = '".$bid."'";
+        $result = mysqli_query($db,$sql);
+        $count = mysqli_num_rows($result);
+
+        if($count == 1){
+            echo 'Exists';
+        } else {
+            $insert = "INSERT INTO $table(bid,eid,pid,bill,businessno,account,type)
+            VALUES ('".$bid."','".$eid."','".$pid."','".$bill."', '".$businessno."', '".$account."' ,'".$type."')";
+            $query = mysqli_query($db,$insert);
+            if($query){
+                echo 'Success';
+            } else {
+                echo 'Failed';
+            }
+        }         
+    }
+
     if('UPDATE' == $action){
         $bid = $_POST['bid'];
         $businessno = $_POST['businessno'];  
