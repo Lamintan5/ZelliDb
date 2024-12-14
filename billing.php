241f589bcd80519fdae7d77c9a32d89578c7001c
Lamintan5 <tech@studio5ive.org>
2024-12-14 10:57:05 +0300

    get current

--- billing.php
+++ billing.php
@@ -55,7 +55,19 @@
         echo json_encode($data);
     }
 
-    
+    if('GET_CURRENT' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $eid = $_POST['eid'];
+        $query = "SELECT * FROM $table WHERE eid = '".$eid."'";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
 
 
     if (isset($action) && $action === 'UPDATE_ACCOUNT') {
