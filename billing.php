d4bc7bee0ccbd61fb54602c609e326c18fadd323
Lamintan5 <tech@studio5ive.org>
2024-12-14 10:55:59 +0300

    updated

--- billing.php
+++ billing.php
@@ -19,9 +19,9 @@
         $pid = $_POST['pid'];
         $bill = $_POST['bill'];
         $businessno = $_POST['businessno'];
-        $account = $_POST['account'];
+        $accountno = $_POST['accountno'];
         $type = $_POST['type'];
-        $checked = $_POST['checked'];
+        $account = $_POST['account'];
 
         $sql = "SELECT bid FROM $table WHERE bid = '".$bid."'";
         $result = mysqli_query($db,$sql);
@@ -30,15 +30,15 @@
         if($count == 1){
             echo 'Exists';
         } else {
-            $insert = "INSERT INTO $table(bid,eid,pid,bill,businessno,account,type,checked)
-            VALUES ('".$bid."','".$eid."','".$pid."','".$bill."', '".$businessno."', '".$account."' ,'".$type."','true')";
+            $insert = "INSERT INTO $table(bid,eid,pid,bill,businessno,accountno,type,account,checked)
+            VALUES ('".$bid."','".$eid."','".$pid."','".$bill."', '".$businessno."', '".$accountno."' ,'".$type."','".$account."','true')";
             $query = mysqli_query($db,$insert);
             if($query){
                 echo 'Success';
             } else {
                 echo 'Failed';
             }
-        }         
+        }
     }
 
     if('GET_MY' == $action){
@@ -55,12 +55,68 @@
         echo json_encode($data);
     }
 
+    if('GET_CURRENT' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $eid = $_POST['eid'];
+        $query = "SELECT * FROM $table WHERE eid = '".$eid."'";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
+
+
+    if (isset($action) && $action === 'UPDATE_ACCOUNT') {
+        $bid = $_POST['bid'];
+        $accountno = $_POST['accountno'];
+    
+        $bid = $conn->real_escape_string($bid);
+        $accountno = $conn->real_escape_string($accountno);
+    
+        $sql = "SELECT `accountno` FROM $table WHERE bid = '$bid'";
+        $result = $conn->query($sql);
+    
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $accField = $row['accountno'];
+    
+            if (empty($accField)) {
+                $newAccField = $accountno;
+            } else {
+                $accsArray = explode('*', $accField);
+
+                if (!in_array($accountno, $accsArray)) {
+                    $newAccField = $accField . '*' . $accountno;
+                } else {
+                    $newAccField = $accField;
+                }
+            }
+    
+            $updateSql = "UPDATE $table SET `accountno` = '$newAccField' WHERE bid = '$bid'";
+            if ($conn->query($updateSql) === TRUE) {
+                echo "success";
+            } else {
+                echo "error" . $conn->error;
+            }
+        } else {
+            echo "Does not exist";
+        }
+    
+        $conn->close();
+    }
+
+
     if('UPDATE' == $action){
         $bid = $_POST['bid'];
         $businessno = $_POST['businessno'];  
+        $accountno = $_POST['accountno'];
         $account = $_POST['account'];
         $type = $_POST['type'];    
-        $sql = "UPDATE $table SET  businessno = '$businessno' WHERE bid = '$bid'";
+        $sql = "UPDATE $table SET  businessno = '$businessno', accountno = '$accountno', account = '$account', type = '$type'  WHERE bid = '$bid'";
         if ($conn->query($sql) === TRUE) { 
             echo "success";
         } else {
